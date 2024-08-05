20781cfaf686a08607f256fb5930083c8a991989
Dhiraj Kumar <132497879+dhirajyadav07@users.noreply.github.com>
2024-08-05 14:23:51 +0530

    Update three_sum_BRUTE_FORCE.cpp

--- three_sum_BRUTE_FORCE.cpp
+++ three_sum_BRUTE_FORCE.cpp
@@ -19,8 +19,8 @@ int main(){
     }
 
     for(int i=0;i<n-2;i++){
-        for(int j=0;j<n-1;j++){
-            for(int k=0;k<n;k++){
+        for(int j=i+1;j<n-1;j++){
+            for(int k=j+1;k<n;k++){
                 if(v[i]+v[j]+v[k]==target){
                     cout<<v[i]<<" "<<v[j]<<" "<<v[k];
                     return 0;
