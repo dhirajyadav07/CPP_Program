fd6514a7ebdbce55aec259fda5134a49219c316c
Dhiraj Kumar <132497879+dhirajyadav07@users.noreply.github.com>
2024-08-05 14:07:58 +0530

    Add files via upload

---
+++ three_sum_BRUTE_FORCE.cpp
@@ -0,0 +1,33 @@
+/*
+Approach 1 :-> Brute Force approach
+time complexity = O(n^3)
+
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n ;
+    cin >>n;
+    int target;
+    cin>>target;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin >>v[i];
+    }
+
+    for(int i=0;i<n-2;i++){
+        for(int j=0;j<n-1;j++){
+            for(int k=0;k<n;k++){
+                if(v[i]+v[j]+v[k]==target){
+                    cout<<v[i]<<" "<<v[j]<<" "<<v[k];
+                    return 0;
+                }
+            }
+        }
+    }
+    return -1;
+
+}
